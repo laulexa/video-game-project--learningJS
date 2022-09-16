dab90adb68a7477acd71e147157ae2c817dc76f4
laulexa <laura.alexandra.roa@gmail.com>
2022-09-16 01:44:33 -0700

    adding more maps

--- maps.js
+++ maps.js
@@ -31,30 +31,30 @@ const emojis = {
     -XXXXXXXXX
     OXXXXXXXXX
   `);
-//   maps.push(`
-//     O--XXXXXXX
-//     X--XXXXXXX
-//     XX----XXXX
-//     X--XX-XXXX
-//     X-XXX--XXX
-//     X-XXXX-XXX
-//     XX--XX--XX
-//     XX--XXX-XX
-//     XXXX---IXX
-//     XXXXXXXXXX
-//     `);
-//   maps.push(`
-//     I-----XXXX
-//     XXXXX-XXXX
-//     XX----XXXX
-//     XX-XXXXXXX
-//     XX-----XXX
-//     XXXXXX-XXX
-//     XX-----XXX
-//     XX-XXXXXXX
-//     XX-----OXX
-//     XXXXXXXXXX
-//   `);
+  maps.push(`
+    O--XXXXXXX
+    X--XXXXXXX
+    XX----XXXX
+    X--XX-XXXX
+    X-XXX--XXX
+    X-XXXX-XXX
+    XX--XX--XX
+    XX--XXX-XX
+    XXXX---IXX
+    XXXXXXXXXX
+    `);
+  maps.push(`
+    I-----XXXX
+    XXXXX-XXXX
+    XX----XXXX
+    XX-XXXXXXX
+    XX-----XXX
+    XXXXXX-XXX
+    XX-----XXX
+    XX-XXXXXXX
+    XX-----OXX
+    XXXXXXXXXX
+  `);
      maps.push(`
     O--------X
     XXXXXXXX-X
